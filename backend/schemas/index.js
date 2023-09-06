1a93c8315e72e6a9c8f5524bf724b10fec136972
Milo Bedini <milobedini64@gmail.com>
2023-09-06 13:38:18 +0100

    sanity set up

--- backend/schemas/index.js
+++ backend/schemas/index.js
@@ -1,3 +1,7 @@
+import comment from './comment'
+import postedBy from './postedBy'
 import user from './user'
+import save from './save'
+import pin from './pin'
 
-export const schemaTypes = [user]
+export const schemaTypes = [user, comment, pin, postedBy, save]
